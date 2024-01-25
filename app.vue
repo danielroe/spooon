840312e8bf504e18487b60928c92d101b5a019b6
Daniel Roe <daniel@roe.dev>
2024-01-25 10:42:34 +0000

    fix: small style tweaks

--- app.vue
+++ app.vue
@@ -7,35 +7,35 @@
       <UBadge v-for="label in recipe.dishTypes" :label="label" :variant="'subtle'" />
     </div>
     <section class="mt-4 flex flex-col md:flex-row-reverse gap-4 w-full">
-      <NuxtImg :src="recipe.image" :alt="recipe.title" class="object-cover w-full" />
+      <NuxtImg v-if="recipe.image" :src="recipe.image" :alt="recipe.title" class="object-cover w-full" />
       <p class="text-lg max-w-[40ch]" v-html="recipe.summary" />
     </section>
     <div class="flex flex-col md:flex-row gap-2 md:gap-8">
-    <section class="py-2 md:py-8 min-w-12">
-      <div class="flex flex-col md:flex-row justify-between md:items-center">
-        <h2 class="text-lg">Ingredients</h2>
-      </div>
-      <UTable class="mt-2" :rows="recipe.extendedIngredients" :columns="columns">
-        <template #name-data="{ row }">
-          <span class="flex flex-row gap-2 items-center">
-            <NuxtImg provider="spoonacular" :src="row.image" :height="20" :width="20" :alt="`Image of ${row.name}`" />
-            {{ row.name }}
-            ({{ [row.measures[unit].amount, row.measures[unit].unitLong].filter(Boolean).join(' ') }})
-          </span>
-        </template>
-      </UTable>
-    </section>
-    <section class="py-2 md:py-8">
-      <h2 class="text-lg">Steps</h2>
-      <ol>
-        <li v-for="{ number, step } in recipe.analyzedInstructions?.[0].steps" class="flex items-baseline gap-2">
-          <span class="flex-shrink-0 inline-flex items-center justify-center rounded-full bg-primary-50 dark:bg-primary-400 dark:bg-opacity-190 text-primary-500 dark:text-primary-400 ring-1 ring-inset ring-primary-500 dark:ring-primary-400 ring-opacity-25 text-xs font-bold h-5 w-5">
-            {{ number }}
-          </span>
-          {{ step }}
-        </li>
-      </ol>
-    </section>
+      <section class="py-2 md:py-8 min-w-12">
+        <div class="flex flex-col md:flex-row justify-between md:items-center">
+          <h2 class="text-lg">Ingredients</h2>
+        </div>
+        <UTable class="mt-2" :rows="recipe.extendedIngredients" :columns="columns">
+          <template #name-data="{ row }">
+            <span class="flex flex-row gap-2 items-center">
+              <NuxtImg v-if="row.image" provider="spoonacular" :src="row.image" :height="20" :width="20" :alt="`Image of ${row.name}`" />
+              {{ row.name }}
+              ({{ [row.measures[unit].amount, row.measures[unit].unitLong].filter(Boolean).join(' ') }})
+            </span>
+          </template>
+        </UTable>
+      </section>
+      <section class="py-2 md:py-8">
+        <h2 class="text-lg">Steps</h2>
+        <ol class="flex flex-col gap-1 md:gap-4 mt-5 w-full max-w-[60ch]">
+          <li v-for="{ number, step } in recipe.analyzedInstructions?.[0].steps" class="flex items-baseline gap-2">
+            <span class="flex-shrink-0 inline-flex items-center justify-center rounded-full text-xs px-2 py-1 bg-primary-50 dark:bg-primary-400 dark:bg-opacity-10 text-primary-500 dark:text-primary-400 ring-1 ring-inset ring-primary-500 dark:ring-primary-400 ring-opacity-25 dark:ring-opacity-25 font-bold h-5 w-5">
+              {{ number }}
+            </span>
+            {{ step }}
+          </li>
+        </ol>
+      </section>
     </div>
     <UButton external :to="recipe.sourceUrl" class="mt-4">
       View original recipe on {{ recipe.sourceName }}
@@ -57,3 +57,9 @@ const columns = columnsToShow.map(col => ({ key: col, label: col }))
 const metric = ref(true)
 const unit = computed(() => metric.value ? 'metric' : 'us')
 </script>
+
+<style>
+p a {
+  @apply text-primary hover:underline;
+}
+</style>
