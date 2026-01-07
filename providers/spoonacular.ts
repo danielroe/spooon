ad6e239b79fa1cf885914e57db683679b2cbe69c
Daniel Roe <daniel@roe.dev>
2026-01-07 11:42:15 +0000

    chore: lint

--- providers/spoonacular.ts
+++ providers/spoonacular.ts
@@ -7,13 +7,13 @@ interface SpoonacularOptions {
 
 export default defineProvider<SpoonacularOptions>({
   getImage: (src, { modifiers = {}, baseURL = 'https://spoonacular.com/cdn' }) => {
-  const sizes = [100, 250, 500]
-  const size = modifiers.width || modifiers.height || 0
-  const nextBiggest = sizes.find((s) => s >= size) || sizes[sizes.length - 1]
-  const prefix = `ingredients_${nextBiggest}x${nextBiggest}`
-  
-  return {
-    url: joinURL(baseURL, prefix, src),
+    const sizes = [100, 250, 500]
+    const size = modifiers.width || modifiers.height || 0
+    const nextBiggest = sizes.find((s) => s >= size) || sizes[sizes.length - 1]
+    const prefix = `ingredients_${nextBiggest}x${nextBiggest}`
+    
+    return {
+      url: joinURL(baseURL, prefix, src),
+    }
   }
-}
 })
